2859759a9eb49b9bcbe9ec7e6fc9e85b4e061fee
SALAHADEEN_DEV <78902162+SALAHADEEN-DEV@users.noreply.github.com>
2024-10-27 15:39:21 +0300

    some

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,6 +2,7 @@
     <!doctype html>
 <html lang="en" dir="rtl">
 <head>
+    helloe
     <meta charset="utf-8">
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">

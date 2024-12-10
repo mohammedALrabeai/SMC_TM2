90b481970422a0418b9c4e47ccd53e73335ef9ef
SALAHADEEN_DEV <78902162+SALAHADEEN-DEV@users.noreply.github.com>
2024-12-10 16:05:12 +0300

    add google tags

--- resources/views/home/ar/layout/wrapper.blade.php
+++ resources/views/home/ar/layout/wrapper.blade.php
@@ -59,6 +59,22 @@
     <script src="{{asset('home/libs/waypoints/jquery.waypoints.min.js')}}"></script>
     <!-- orther script -->
     <script src="{{asset('home/js/main.js')}}"></script>
+
+    <!-- Google Tag Manager -->
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-W4KNTBKT');</script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body>
@@ -259,5 +275,12 @@
         </div><!-- .container -->
     </footer><!-- site-footer -->
 </div><!-- #wrapper -->
+
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W4KNTBKT"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
 </body>
 </html>
